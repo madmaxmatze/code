c0366bbfb5927a5174252e1a05853450cfed6b79
madmaxmatze <madmaxmatze@users.noreply.github.com>
2022-11-23 13:29:05 +0100

    Update userscript.js

--- shell/userscript.js
+++ shell/userscript.js
@@ -1,30 +1,29 @@
 // ==UserScript==
-// @name         Google Cloud Shell - Checker
+// @name         Google Cloud Shell - Quota Checker
 // @namespace    https://mathiasnitzsche.de
-// @version      1.1
-// @description  Script to check remaining time within Google Cloud Shell quota to avoid being locked out
+// @version      1.0
+// @description  Script to check remaining time within Google Cloud Shell Quota (50h/week) to avoid being locked
 // @author       Mathias Nitzsche
 // @match        https://shell.cloud.google.com/*
 // @icon         https://shell.cloud.google.com/favicon.ico
-// @grant        none
-// @require      https://code.mathiasnitzsche.de/shell/userscript.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    function checkTime () {
+    function checkTime() {
         fetch('https://shell.cloud.google.com/devshell/quota').then(response => {
             response.text().then(textResult => {
                 var [remainingSecs, totalSecs, endDate] = JSON.parse(textResult.split("\n")[1]).flat();
                 var remainingHours = remainingSecs / 60 / 60;
                 console.log("Cloud Quota checked. Remaining hours: ", remainingHours.toFixed(1));
                 if (remainingHours < 3) {
-                    alert("Less then 3 hours left");
+                    alert(remainingHours + " hours left before Google Cloud Shell Quota is exceeded");
                 }
             });
         });
+        return checkTime;
     }
 
-    setInterval(checkTime, 1000 * 60 * 30); // check every 30mins
+    setInterval(checkTime(), 1000 * 60 * 10); // check every 30mins
 })();
