1f082d160ab11714bf6f0fa3602238e9c047fc82
madmaxmatze <madmaxmatze@users.noreply.github.com>
2022-11-23 11:45:32 +0100

    Update userscript.js

--- shell/userscript.js
+++ shell/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  loaded from github
 // @author       You
 // @match        https://shell.cloud.google.com/?show=ide%2Cterminal
