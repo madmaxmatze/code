6562143019d4af6db0e2474576d3b21bb2f8f327
madmaxmatze <madmaxmatze@users.noreply.github.com>
2022-11-23 12:04:21 +0100

    Update userscript.js

--- shell/userscript.js
+++ shell/userscript.js
@@ -1,36 +1,33 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  loaded from github
-// @author       You
-// @match        https://shell.cloud.google.com/?show=ide%2Cterminal
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @name         Google Cloud Shell - Checker
+// @namespace    https://mathiasnitzsche.de
+// @version      1.0
+// @description  Script to check remaining time within Google Cloud Shell quota to avoid being locked out
+// @author       Mathias Nitzsche
+// @match        https://shell.cloud.google.com/*
+// @icon         https://shell.cloud.google.com/favicon.ico
 // @grant        none
+// @require      https://code.mathiasnitzsche.de/shell/userscript.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var counter = JSON.parse(localStorage.getItem('test'));
-    if (!counter) {
-        counter = 0;
+    function checkTime () {
+        fetch('https://shell.cloud.google.com/devshell/quota?authuser=0').then(response => {
+            // console.log(JSON.stringify(response) );
+            // response.text().then(result => {return console.log(result);});
+            response.text().then(result => {
+                var [remainingSecs, totalSecs, endDate] = JSON.parse(result.split("\n")[1]).flat()
+                console.log("remainingSecs", remainingSecs);
+                console.log("totalSecs", totalSecs);
+                console.log("endDate", endDate);
+                if (remainingSecs < 3 * 3600) {
+                    alert("Less then 3 hours left");
+                }
+            });
+        });
     }
 
-    counter++;
-    localStorage.setItem('test', counter);
-    console.log (counter);
-
-    fetch('https://shell.cloud.google.com/devshell/quota?authuser=0').then(response => {
-         // console.log(JSON.stringify(response) );
-        // response.text().then(result => {return console.log(result);});
-        response.text().then(result => {
-            var [remainingSecs, totalSecs, endDate] = JSON.parse(result.split("\n")[1]).flat()
-            console.log("remainingSecs", remainingSecs);
-            console.log("totalSecs", totalSecs);
-            console.log("endDate", endDate);
-
-        });
-    });
-
+    setInterval(checkTime, 5000);
 })();
