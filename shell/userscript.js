f679428533578012dd4f1b17bdc210fcbdc60ca5
madmaxmatze <madmaxmatze@users.noreply.github.com>
2022-11-27 11:54:22 +0100

    Redirect added

--- shell/userscript.js
+++ shell/userscript.js
@@ -1,12 +1,10 @@
-// source: https://github.com/madmaxmatze/code/tree/main/shell
-// UserScript for tampermonkey.net chrome extension
-
 // ==UserScript==
 // @name         Google Cloud Shell - Quota Checker
 // @namespace    https://mathiasnitzsche.de
-// @version      1.0
-// @description  Script to check remaining time within Google Cloud Shell Quota (50h/week) to avoid being locked
+// @version      1.1
+// @description  UserScript for tampermonkey.net chrome extension to check the remaining time within Google Cloud Shell Quota (50h/week) to avoid being locked
 // @author       Mathias Nitzsche
+// @source       https://github.com/madmaxmatze/code/tree/main/shell
 // @match        https://shell.cloud.google.com/*
 // @icon         https://shell.cloud.google.com/favicon.ico
 // @noframes
@@ -20,16 +18,18 @@
     function checkTime() {
         fetch(`/devshell/quota?authuser=${authuser}`)
             .then(response => response.text())
-            .then(textResult => textResult.match(/(\d+)/).shift() / 3600)
-            .then(remainingHours => {
-                 var message = `Cloud Quota checked. Hours remaining: ${remainingHours.toFixed(1)}`;
+            .then(textResult => JSON.parse(textResult.match(/(\[.+\])/g)).flat())
+            .then(([remainingSeconds, _totalSeconds, endTimestamp]) => {
+                 var remainingHours = Math.round(remainingSeconds / 3600 * 10) / 10;
+                 var message = `Cloud Quota checked\n- ${remainingHours} hours are remaining\n- Reset will happen on ${(new Date(endTimestamp)).toLocaleString()}`;
                  console.log(message);
-                 if (remainingHours < 5) {
-                     alert(message);
+                 if (remainingHours < 3) {
+                     alert(message + "\n- This page will redirect in 5mins.");
+                     setTimeout(() => {window.location.assign("https://www.google.com")}, 60000 * 5/*min*/);
                  }
              });
     }
 
     checkTime();
-    setInterval(checkTime, 1000 * 60 * 30); // every 30mins
+    setInterval(checkTime, 60000 * 30/*min*/);
 })();
