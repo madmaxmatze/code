50926e1a4b4aa7c04da2878c7c2900ff007fdb59
madmaxmatze <madmaxmatze@gmail.com>
2023-01-09 21:39:45 +0000

    added comment

--- proxy/worker.js
+++ proxy/worker.js
@@ -8,45 +8,50 @@
  * Source: https://github.com/madmaxmatze/code/tree/main/proxy
  **/
 addEventListener('fetch', event => event.respondWith(
-    getUrlParam(event.request.url)
-      .then(checkIfWhitelisted)
-      .then(fetch)
-      .then(checkFetchError)
-      // for now handled with Cloudflare header transform rule
-      // .then(extendResponseWithCorsHeader)
-      .catch(handleError)
-  ));
-  
-  const getUrlParam = async (fullRequestUrl/*:string*/) => {
-    try {
-      return new URL((new URL(fullRequestUrl)).searchParams.get('url'));
-    } catch (err) {
-      throw URIError("?url= parameter is missing or invalid");
-    }
-  };
-  
-  const checkIfWhitelisted = async (url/*:URL*/) => {
-    if (!(DOMAIN_WHITELIST || "").split("|").includes(url.hostname)) {
-      throw URIError("Domain not whitelisted");
-    }
-    return url.href;
-  };
-  
-  const checkFetchError = async (response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response;
-  };
-  
-  const handleError = async (error) => (
-    new Response(error.toString(), {status: error instanceof URIError ? 400 : 500})
-  );
-  
-  /*
-  const extendResponseWithCorsHeader = async (response) => {
-    let mutableResponse = new Response(response.body, response);
-    mutableResponse.headers.append("Access-Control-Allow-Origin", "*");
-    return mutableResponse;
-  };
-  */
+  getUrlParam(event.request.url)
+    .then(checkIfWhitelisted)
+    .then(fetch)
+    .then(checkFetchError)
+    .catch(handleError)
+));
+
+const getUrlParam = async (fullRequestUrl/*:string*/) => {
+  try {
+    return new URL((new URL(fullRequestUrl)).searchParams.get('url'));
+  } catch (err) {
+    throw URIError("?url= parameter is missing or invalid");
+  }
+};
+
+const checkIfWhitelisted = async (url/*:URL*/) => {
+  if (!(DOMAIN_WHITELIST || "").split("|").includes(url.hostname)) {
+    throw URIError("Domain not whitelisted");
+  }
+  return url.href;
+};
+
+const checkFetchError = async (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+const handleError = async (error) => (
+  new Response(error.toString(), { status: error instanceof URIError ? 400 : 500 })
+);
+
+
+/*
+ * CORS header needed to allow servive consumption from browsers
+ * Function can add header via .then(extendResponseWithCorsHeader)
+ * Not needed bcause set by Cloudflare transform rule:
+ * -> https://developers.cloudflare.com/rules/transform/response-header-modification/
+ */
+/*
+const extendResponseWithCorsHeader = async (response) => {
+  let mutableResponse = new Response(response.body, response);
+  mutableResponse.headers.append("Access-Control-Allow-Origin", "*");
+  return mutableResponse;
+};
+*/
