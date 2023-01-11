dc5075d6ff130d305a80696f558c4b85d83cfe63
madmaxmatze <madmaxmatze@gmail.com>
2023-01-11 09:41:55 +0000

    hashnode rest api

---
+++ hasnode-api/README.md
@@ -0,0 +1,9 @@
+# Hashnode Rest API
+
+A simpler Rest API Facade for the fancy [Hashnode GraphQL API](https://api.hashnode.com/)
+
+Deployed as [Cloudflare Worker](https://workers.cloudflare.com/)
+
+Used on mathiasnitzsche.de
+
+ * **Usage:** /api/user/[user]/posts
---
+++ hasnode-api/worker.js
@@ -0,0 +1,59 @@
+/**
+ * Author:    Mathias Nitzsche
+ * Created:   2022-01-11
+ * 
+ * Rest API for Hashnode - deployed as Cloudflare Worker
+ * 
+ * Usage: /api/user/[user]/posts
+ * Source: https://github.com/madmaxmatze/code/tree/main/hashnode-api
+ **/
+addEventListener('fetch', event => event.respondWith(
+    handleRequest(event.request.url)
+      .then(checkFetchError)
+      .catch(handleError)
+  ));
+  
+  const handleRequest = async (url/*:string*/) => {
+    url = url.replace(/\?.*/, "");
+    var match = url.match(/api\/v1\/user\/(\w+)\/posts$/);
+    if (match) {
+      return fetchPosts(match[1]);
+    }
+    
+    throw URIError("API call invalid");
+  };
+  
+  const fetchPosts = async (username/*:string*/) => {
+     var query = `{
+      user(username: "${username}") {
+        blogHandle
+        publicationDomain
+        publication {
+          posts(page: 0) {
+            dateAdded
+            slug
+            title
+            brief
+            coverImage
+          }
+        }
+      }
+    }`;
+    
+    return fetch('https://api.hashnode.com/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query })
+    });
+  };
+  
+  const checkFetchError = async (response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  };
+  
+  const handleError = async (error) => (
+    new Response(error.toString(), { status: error instanceof URIError ? 400 : 500 })
+  );
